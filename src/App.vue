200823698aa9fe88388c0fe810cec40f0620bd4c
Jaime Arias <arias@lipn.univ-paris13.fr>
2026-06-24 08:29:04 +0200

    add for to the list

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,17 @@ import { ref } from 'vue';
 
 const isEmpty = ref(false);
 
+const tasks = ref([
+  { id: 1, title: 'Configure the laptop', done: true },
+  { id: 2, title: 'Ask for some feedback', done: false },
+]);
+
 function addTask(){
   alert('Add task');
 }
 
-function deleteTask(){
-  alert('Delete task');
+function deleteTask(id: number){
+  alert(`Delete task: ${id}`);
 }
 
 function filterTasks(filter: string){
@@ -41,10 +46,10 @@ function filterTasks(filter: string){
       <!-- Task list -->
       <ul class="task-list">
 
-        <li class="task-item task-item--done">
-          <input type="checkbox" class="task-checkbox" checked />
-          <span class="task-label task-label--done">Configure the laptop</span>
-          <button class="btn-delete" aria-label="Delete task" @click="deleteTask">
+        <li v-for="task in tasks" class="task-item task-item--pending" :key="task.id">
+          <input type="checkbox" class="task-checkbox" :checked="task.done" />
+          <span class="task-label task-label--pending">{{ task.title }}</span>
+          <button class="btn-delete" aria-label="Delete task" @click="deleteTask(task.id)">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <polyline points="3 6 5 6 21 6" />
@@ -54,21 +59,6 @@ function filterTasks(filter: string){
             </svg>
           </button>
         </li>
-
-        <li class="task-item task-item--pending">
-          <input type="checkbox" class="task-checkbox" />
-          <span class="task-label">Ask for some feedback</span>
-          <button class="btn-delete" aria-label="Delete task" @click="deleteTask">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <polyline points="3 6 5 6 21 6" />
-              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-              <line x1="10" y1="11" x2="10" y2="17" />
-              <line x1="14" y1="11" x2="14" y2="17" />
-            </svg>
-          </button>
-        </li>
-
       </ul>
     </div>
   </div>
